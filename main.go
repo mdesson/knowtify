1cc40d57ace877e7af490b18be2884d7d46ccf2c
Michael Desson <michael.desson@gmail.com>
2021-06-11 23:17:51 -0400

    Finished initial version

--- main.go
+++ main.go
@@ -46,11 +46,13 @@ type Product struct {
 }
 
 type Config struct {
-	TwilioSID   string    `json:"twilioSID"`
-	TwilioToken string    `json:"twilioToken"`
-	SendTo      []string  `json:"sendTo"`
-	SendFrom    string    `json:"sendFrom"`
-	Products    []Product `json:"products"`
+	EndHour      int           `json:"endHour"`
+	SleepSeconds time.Duration `json:"sleepSeconds"`
+	TwilioSID    string        `json:"twilioSID"`
+	TwilioToken  string        `json:"twilioToken"`
+	SendTo       []string      `json:"sendTo"`
+	SendFrom     string        `json:"sendFrom"`
+	Products     []*Product    `json:"products"`
 }
 
 // Response is a Twilio sms response to be sent as xml
@@ -60,30 +62,46 @@ type Response struct {
 }
 
 // SendText sends an sms message to the specified number
-func sendStockNotification(c Config, msg, sid, token, to, from string) int {
+func sendStockNotification(c Config) int {
 	// Config for text message
 	data := url.Values{}
-	data.Set("To", to)
-	data.Set("From", from)
-	data.Set("Body", msg)
 
-	msgURL := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%s/Messages.json", sid)
+	var respCode int
 
-	// Set up request
-	r, _ := http.NewRequest(http.MethodPost, msgURL, strings.NewReader(data.Encode()))
-	r.SetBasicAuth(sid, token)
-	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	msg := "🚨 STOCK ALERT 🚨"
+	for _, product := range c.Products {
+		if product.InStock {
+			msg = fmt.Sprintf("%s\n%s %s\n\n", msg, product.Name, product.URL)
+		}
+	}
 
-	// Send Request
-	client := &http.Client{}
-	resp, _ := client.Do(r)
+	for _, recipient := range c.SendTo {
+		data.Set("To", recipient)
+		data.Set("From", c.SendFrom)
+		data.Set("Body", msg)
 
-	return resp.StatusCode
+		msgURL := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%s/Messages.json", c.TwilioSID)
+
+		// Set up request
+		r, _ := http.NewRequest(http.MethodPost, msgURL, strings.NewReader(data.Encode()))
+		r.SetBasicAuth(c.TwilioSID, c.TwilioToken)
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+		// Send Request
+		client := &http.Client{}
+		resp, _ := client.Do(r)
+		respCode := resp.StatusCode
+
+		if respCode >= 400 {
+			break
+		}
+	}
+
+	return respCode
 }
 
 func main() {
-
 	configFile, err := os.Open("config.json")
 	if err != nil {
 		log.Fatal(err)
@@ -97,33 +115,56 @@ func main() {
 
 	var config Config
 	json.Unmarshal(configBytes, &config)
-	fmt.Println(config)
 
-	for _, product := range products {
-		req, err := http.NewRequest("GET", product.URL, nil)
-		if err != nil {
-			log.Fatal(err)
+	products := config.Products
+
+	fmt.Println("Loaded config, starting job.")
+
+	for time.Now().Hour() < config.EndHour {
+		productsAreInStock := false
+
+		for _, product := range products {
+			fmt.Printf("Checking if %v is in stock\n", product.Name)
+			req, err := http.NewRequest("GET", product.URL, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			req.Header.Add("User-Agent", randomUserAgent())
+			req.Header.Add("Accept", "text/html, */*; q=0.01")
+			req.Header.Add("Accept-Language", "en-CA,en-US;q=0.7,en;q=0.3")
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			body, _ := io.ReadAll(resp.Body)
+
+			if !strings.Contains(string(body), `<p class="product-out-of-stock">Out of stock</p>`) {
+				product.InStock = true
+				fmt.Println("Product in Stock!")
+				if !productsAreInStock {
+					productsAreInStock = true
+				}
+			}
+		}
+		fmt.Println("Checking complete")
+
+		if productsAreInStock {
+			fmt.Printf("Found %v products, sending...\n", len(config.Products))
+			res := sendStockNotification(config)
+			if res >= 300 {
+				log.Fatalf("SMS completed with error %v. Aborting job.\n", res)
+			}
+			fmt.Println("Sending complete.")
+		} else {
+			fmt.Println("Nothing to send")
 		}
 
-		req.Header.Add("User-Agent", randomUserAgent())
-		req.Header.Add("Accept", "text/html, */*; q=0.01")
-		req.Header.Add("Accept-Language", "en-CA,en-US;q=0.7,en;q=0.3")
-
-		resp, err := http.DefaultClient.Do(req)
-		// resp, err := http.Get(URL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-
-		body, _ := io.ReadAll(resp.Body)
-
-		if strings.Contains(string(body), `<p class="product-out-of-stock">Out of stock</p>`) {
-			product.InStock = true
-		}
+		fmt.Printf("Batch Sent. Sleeping for %v seconds.\n", config.SleepSeconds)
+		time.Sleep(config.SleepSeconds * time.Second)
 	}
-
-	for _, product := range products {
-		fmt.Println(product.InStock)
-	}
+	fmt.Println("Complete!")
 }
